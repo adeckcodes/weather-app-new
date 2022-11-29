fbbe4cd4a9d753c13d31eab8b2219c891a127fd6
adeckcodes <109977607+adeckcodes@users.noreply.github.com>
2022-11-28 23:57:41 -0600

    connected API to weekly forecast

--- script.js
+++ script.js
@@ -24,7 +24,8 @@ function formattedDate(date) {
   return `${days[dayIndex]} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -52,8 +53,13 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  let apiKey = `f5029b784306910c19746e40c14d6cd3`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayWeatherCondition(response) {
-  console.log(response.data);
   let iconElement = document.querySelector("#icon");
 
   document.querySelector("#city").innerHTML = response.data.name;
@@ -71,6 +77,7 @@ function displayWeatherCondition(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
@@ -129,5 +136,5 @@ let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
 searchCity("New York");
-displayForecast();
+
 
